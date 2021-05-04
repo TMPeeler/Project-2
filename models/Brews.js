4ca59bb4236aeda6b5cbc3ee6d59e81fcb6f03de
markosanchez800 <markosanchez800@gmail.com>
2021-05-04 13:28:52 -0700

    added brew model

---
+++ models/Brews.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Brew extends Model {}
+
+Brew.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        alcContent:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        typeOfBrew:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // score:{
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     validate:{
+        //         min: 1,
+        //         max: 5,
+        //     },
+        // },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'brew',
+      }
+);
+
+module.exports = Brew;
