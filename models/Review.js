976566f23257290f8fea8ddb61f78a107b0abe01
markosanchez800 <markosanchez800@gmail.com>
2021-05-04 13:24:26 -0700

    added review model

---
+++ models/Review.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        review:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        score:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                min: 1,
+                max: 5,
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+      }
+);
+
+module.exports = Review;
