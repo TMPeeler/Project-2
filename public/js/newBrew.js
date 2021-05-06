cd94f2e3663602c13004afec290470b158411013
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-05 20:22:33 -0700

    add scripts for both new Brews and new Reviews

--- public/js/newBrew.js
+++ public/js/newBrew.js
@@ -1,18 +1,27 @@
-const newPosting = async (event) => {
+const newBrew = async (event) => {
     event.preventDefault();
     const title = document.querySelector('#newp-title').value.trim();
-    const content = document.querySelector('#newp-body').value.trim();
-    console.log('function running');
-    if (title && content){
-      const response = await fetch('/api/post/newp',{
+    const description = document.querySelector('#desc').value.trim();
+    const brewery = document.querySelector('#brewery').value.trim();
+    const alcohol = document.querySelector('#alcohol').value.trim();
+    const style = document.querySelector('#style').value.trim();
+
+
+    if (title && description && brewery && alcohol && style ){
+        // change this to whatever the name of the route with the specific beer is called
+      const response = await fetch('',{
         method:'POST',
-        body: JSON.stringify({title,content}),
+        body: JSON.stringify({title, description, brewery, alcohol, style,}),
         headers: {'Content-Type': 'application/json'},
       });
       if (response.ok) {
-        document.location.replace('/api/post/dashboard');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
     }
   };
+
+  document
+  .querySelector('.new-beer-form')
+  .addEventListener('submit', newBrew);
