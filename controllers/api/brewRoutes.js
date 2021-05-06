584df9ac1be757be7382b513ca728d974ca749b8
markosanchez800 <markosanchez800@gmail.com>
2021-05-06 09:32:52 -0700

    only one card now renders when clicked on

--- controllers/api/brewRoutes.js
+++ controllers/api/brewRoutes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req,res) => {
         console.log(brewData);
         //res.status(200).json(brewData);
         res.render('brewpage',{
-            brew : brewData,
+            brew : brewData.dataValues,
             logged_in: req.session.logged_in,
         });
     } catch (err) {
