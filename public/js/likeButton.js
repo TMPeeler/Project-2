099bee78a8a7e717d7b48f6a57af1a9136fa3df3
javimarashall <jamondragon@ucdavis.edu>
2021-05-06 17:27:38 -0700

    like button function

--- public/js/likeButton.js
+++ public/js/likeButton.js
@@ -1,1 +1,8 @@
 //Like button increment
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
