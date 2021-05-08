ceb4d90a739e02c3de6960c4755db848f06ffeb2
javimarashall <jamondragon@ucdavis.edu>
2021-05-07 19:31:00 -0700

    drop down menu

--- public/js/likeButton.js
+++
@@ -1,9 +0,0 @@
-// var buttonTarget = document.getElementsByClassName("btn")[0];
-// var counter = 1;
-
-// function incrementer(){
-// //document.getElementById("btn").innerHTML = counter.toString();
-//   buttonTarget.innerHTML  = counter.toString();
-//   counter++;
-//   return counter;
-// }
